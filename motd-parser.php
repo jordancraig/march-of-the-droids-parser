1124623d70c2d8bcb147b0b8a8e31cc753c0f20f
Jordan Craig <jordan.craig94@gmail.com>
2013-01-14 21:11:46 +0000

    Rewrite JSON creation method
    Allows the app to read in and parse the JSONObjects passed to it.

--- motd-parser.php
+++ motd-parser.php
@@ -8,32 +8,26 @@ global $date_array;
 global $author_array;
 
 
-scrape_article_info();
 
-
-function scrape_article_info() {
-
-
-    $rss_feed = simplexml_load_file('http://www.marchofthedroids.co.uk/feed/');
+$rss_feed = simplexml_load_file('http://www.marchofthedroids.co.uk/feed/');
      
-    foreach($rss_feed->channel->item as $article) {
-      $title_array[] = $article->title;         //article title
-      $link_array[] = $article->link;          //article link
-      $description_array[] = $article->description;   //article desc
-      $date_array[] = $article->pubDate;       //article date
+foreach($rss_feed->channel->item as $article) {
+
+    $title_array[] = $article->title;        //article title
+    $description = $article->description;
+    $description = substr($description, 0, -148);
+    $link_array[] = $article->link;          //article link
+    $description_array[] = $description . "...";   //article desc
+    $date_array[] = $article->pubDate;       //article date
            
-      //get namespaced items
-      $namespaces = $article->getNameSpaces(true);
-      $dc = $article->children($namespaces['dc']);
-      $author_array[] = $dc->creator;            //article author
+    //get namespaced items
+    $namespaces = $article->getNameSpaces(true);
+    $dc = $article->children($namespaces['dc']);
+    $author_array[] = $dc->creator;            //article author
   
-    }
-
-    create_json_document($title_array, $link_array, $description_array, $date_array, $author_array);
-
 }
 
-
+$json = create_json_document($title_array, $link_array, $description_array, $date_array, $author_array);
 
 function create_json_document($title_array, $link_array, $description_array, $date_array, $author_array) {
     
@@ -43,8 +37,8 @@ function create_json_document($title_array, $link_array, $description_array, $da
 
     while($i < $l) {
         
-        $jsontext .= '"article":' . "{" . '"title":'  . '"' .
-                        $title_array[$i] . '"' . ',' .  '"author":' . '"' . $author_array[$i] . '"' . "},";
+        $jsontext .= '"article ' . $i . '":' . " [ { " . '"title":'  . '"' .
+                        $title_array[$i] . '"' . ',' .  '"desc":' . '"' . $description_array[$i] . '"}' . "],";
 
         $i++;
 
@@ -52,8 +46,10 @@ function create_json_document($title_array, $link_array, $description_array, $da
     
     $jsontext = substr_replace($jsontext, '', -1); // to get rid of extra comma
     $jsontext .= '} }';
-    echo $jsontext;
+    return $jsontext;
 
 }
+
+print $json;
 
 ?>
